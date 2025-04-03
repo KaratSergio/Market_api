e426769d4fae62bf8b59c22b105f7fe36ae99163
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2025-04-03 19:16:29 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:5173', 'https://market-nu-eight.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
